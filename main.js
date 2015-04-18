96ebc14b06b9ffbc00c7fa797add77766991428e
Amit Kulkarni <kulkarniar@hotmail.com>
2015-04-17 23:42:54 -0700

    Removed useless {}

--- main.js
+++ main.js
@@ -92,9 +92,9 @@ function throughputCalculator(){
   };
 
   db.putItem(putparams, function(err, data){
-    if (err) console.log(err, err.stack); // an error occurred
-    else     
-      console.log(data);           // successful response
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+    }    
   });
 
   dataProcessed = 0;
